b3d3284f22daab6fd7ecb171ae839c09caa8f6c6
jwhjkl0720-alt <jwhjkl0720@gmail.com>
2026-01-05 18:32:15 +0900

    01/05fastapi_task

---
+++ 04_fastapi_task/app/apis/v1/meeting_router.py
@@ -0,0 +1,16 @@
+from app.dtos.get_meeting_response import GetMeetingResponse
+
+
+@edgedb_router.get(
+    "/{meeting_url_code}",
+    description="meeting 을 조회합니다.",
+)
+async def api_get_meeting_edgedb(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
+
+@mysql_router.get(
+    "/{meeting_url_code}",
+    description="meeting 을 조회합니다.",
+)
+async def api_get_meeting_mysql(meeting_url_code: str) -> GetMeetingResponse:
+    return GetMeetingResponse(url_code="abc")
